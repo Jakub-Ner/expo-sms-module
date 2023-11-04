de62a699a8da0b9a75aeaa44d8661161712825bd
jakub-ner <kubaner1@gmail.com>
2023-11-04 14:53:04 +0100

    hello world

---
+++ README.md
@@ -0,0 +1,8 @@
+### Development
+Kotlin files to be edited are located in 'android/src/main/java/expo/modules/smsmodule/'
+
+### Running
+within `example/` run 
+```
+npx expo run:android --device
+```
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,6 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
-// Import the native module. On web, it will be resolved to ExpoSmsModule.web.ts
-// and on native platforms to ExpoSmsModule.ts
 import ExpoSmsModule from './ExpoSmsModule';
-import ExpoSmsModuleView from './ExpoSmsModuleView';
-import { ChangeEventPayload, ExpoSmsModuleViewProps } from './ExpoSmsModule.types';
-
-// Get the native constant value.
-export const PI = ExpoSmsModule.PI;
 
 export function hello(): string {
   return ExpoSmsModule.hello();
 }
 
-export async function setValueAsync(value: string) {
-  return await ExpoSmsModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ExpoSmsModule ?? NativeModulesProxy.ExpoSmsModule);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ExpoSmsModuleView, ExpoSmsModuleViewProps, ChangeEventPayload };
