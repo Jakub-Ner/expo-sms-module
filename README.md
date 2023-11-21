f771c822909997c9c9b99342e1f1687ddc9a799c
jakub-ner <kubaner1@gmail.com>
2023-11-21 16:47:15 +0100

    linting and cleaning

--- README.md
+++ README.md
@@ -1,15 +1,44 @@
+# Expo SMS Module
+
+Expo module for sending and receiving SMS messages on Android.
+
+### Usage:
+
+#### Available functions:
+```ts
+async function requestSendSmsPermissionsAsync() {
+  return ExpoSmsModule.requestSendSmsPermissionsAsync();
+}
+
+function sendSms(phoneNumber: string, message: string): void {
+  ExpoSmsModule.sendSms(phoneNumber, message);
+}
+
+const emitter = new EventEmitter(ExpoSmsModule);
+
+function addSmsListener(
+  listener: (event: { message: string; phoneNumber: string }) => void,
+): Subscription {
+  return emitter.addListener("onSMSReceived", listener);
+}
+```
+
+See example usage in [example/App.tsx](example/App.tsx)
+
+
+
 ### Development
 Files to be edited are located in:
 - 'android/src/main/java/expo/modules/smsmodule/'
 - 'src/'
 
-### Running
+#### Running Example
 within `example/` run 
 ```
 npx expo run:android --device <Device-Name>
 ```
 
-### Debugging:
+#### Debugging:
 Assuming:
 ```.kt
 import android.util.Log
--- example/App.tsx
+++ example/App.tsx
@@ -1,24 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-// import {hello} from 'expo-sms-module';
-import * as ExpoSmsModule from 'expo-sms-module';
+import {
+  requestSendSmsPermissionsAsync,
+  sendSms,
+  addSmsListener,
+} from "expo-sms-module";
+import * as ExpoSmsModule from "expo-sms-module";
 import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  console.log("App testing");
-  ExpoSmsModule.requestSendSmsPermissionsAsync().then((result) => {
-      console.log(result);
-      // ExpoSmsModule.sendSms('+12 345 678 999', 'Hello from Expo'); // uncomment and change phone number 
-  }).catch(
-    (error) => {
-      console.log(error);
-    }
-  )
-
   const [message, setMessage] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+
   useEffect(() => {
-    const sub = ExpoSmsModule.addSmsListener(({ message, phoneNumber }) => {
+    requestSendSmsPermissionsAsync()
+      .then((result) => {
+        console.log(result);
+        // sendSms("+12 345 678 900", "Hello from Expo"); // uncomment and change phone number
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    const sub = addSmsListener(({message, phoneNumber}) => {
       setMessage(message);
       setPhoneNumber(phoneNumber);
     });
@@ -38,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
